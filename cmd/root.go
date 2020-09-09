a712e81bcd7f9b63fa014a6b55683efd6ff484b8
Raphael Pour <info@raphaelpour.de>
2020-09-09 17:18:45 +0200

    Cmd: Init cobra arg parser

---
+++ cmd/root.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2020 Raphael Pour <info@raphaelpour.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "blogctl",
+	Short: "Static markdown blog backend",
+	Long:  "Blogctl manages blog markdown-based posts database-less and generates a static website on-demand",
+	Run: func(cmd *cobra.Command, args []string) {
+		if *Version {
+			fmt.Printf("BuildVersion: %s", BlogVersion)
+			fmt.Printf("BuildDate: %s", BlogDate)
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+const DEFAULT_BLOG_PATH = "./blog/"
+
+var (
+	BuildDate    string
+	BuildVersion string
+	BlogPath     string
+	Version      bool
+)
+
+func init() {
+
+	rootCmd.PersistentFlags().StringVar(
+		&BlogPath,
+		"path",
+		DEFAULT_BLOG_PATH,
+		fmt.Sprintf("Path to blog content. Default: %s", DEFAULT_BLOG_PATH),
+	)
+}

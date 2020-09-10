9b8e21c3dd4cb3b1829ed298e7f544f12a3dd10c
Raphael Pour <info@raphaelpour.de>
2020-09-10 20:31:22 +0200

    Repo: Update github account
    CI: Add travis config

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package main
 
-import "3vilcookie/blogctl/cmd"
+import "github.com/RaphaelPour/blogctl/cmd"
 
 func main() {
 	cmd.Execute()

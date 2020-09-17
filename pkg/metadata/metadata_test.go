a253df47d35b84ebdd312983320f9a3dce96254c
Raphael Pour <raphael.pour@hetzner.com>
2020-09-17 12:52:51 +0200

    Testing: Add metadata tests

---
+++ pkg/metadata/metadata_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadMetadata(t *testing.T) {
+}
+
+func TestNewInvalidMetadata(t *testing.T) {
+}
+
+func TestSaveMetadata(t *testing.T) {
+}
+
+func TestMetadataFile(t *testing.T) {
+	require.Equal(t, GetMetadataPath("/tmp"), "/tmp/metadata.json")
+}

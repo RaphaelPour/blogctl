eb9fe3bdc939c6b048140d6c3c77b60218d163ac
Raphael Pour <info@raphaelpour.de>
2020-09-18 00:17:03 +0200

    Cmd: Add render command
    Cmd: Add interactive flag, print created post file

---
+++ cmd/render.go
@@ -0,0 +1,160 @@
+/*
+Copyright © 2020 Raphael Pour <info@raphaelpour.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/RaphaelPour/blogctl/pkg/metadata"
+	"github.com/gomarkdown/markdown"
+	"github.com/spf13/cobra"
+)
+
+type Post struct {
+	Content  string
+	Rendered template.HTML
+	Metadata *metadata.Metadata
+}
+
+// renderCmd represents the render command
+var renderCmd = &cobra.Command{
+	Use:   "render",
+	Short: "Renders blog to static website",
+	Long:  "Collects all posts and renders the markdown using the metadata as static website",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if _, err := os.Stat(OutPath); !os.IsNotExist(err) && !Force {
+			return fmt.Errorf("Output folder already exists")
+		}
+
+		if err := os.MkdirAll(OutPath, os.ModePerm); err != nil {
+			return fmt.Errorf("Error creating output folder: %s", err)
+		}
+
+		postDirs, err := ioutil.ReadDir(BlogPath)
+		if err != nil {
+			return fmt.Errorf("Error reading blog path: %s", err)
+		}
+
+		posts := make([]Post, 0)
+		for i, dir := range postDirs {
+			fmt.Printf("Processing post #%02d: %s\n", i, dir.Name())
+
+			if !dir.IsDir() {
+				continue
+			}
+
+			postPath := filepath.Join(BlogPath, dir.Name())
+			files, err := ioutil.ReadDir(postPath)
+			if err != nil {
+				return fmt.Errorf("Error reading post path of %s: %s", postPath, err)
+			}
+
+			if len(files) != 2 {
+				return fmt.Errorf(
+					"Unexpected count of files in post path %s. Found: %d",
+					postPath,
+					len(files),
+				)
+			}
+
+			metadata, err := metadata.LoadMetadata(postPath)
+			if err != nil {
+				return err
+			}
+
+			content, err := ioutil.ReadFile(GetContentFile(postPath))
+			if err != nil {
+				return fmt.Errorf("Error reading post content %s: %s", postPath, err)
+			}
+
+			rendered := markdown.ToHTML(content, nil, nil)
+
+			posts = append(posts, Post{
+				Metadata: metadata,
+				Content:  string(content),
+				Rendered: template.HTML(rendered),
+			})
+		}
+
+		/* Put everything together */
+		t, err := template.New("blog").Parse(TEMPLATE)
+		if err != nil {
+			fmt.Errorf("Error parsing the html template: %s", err)
+		}
+
+		/* Save site to out dir  */
+		sitePath := filepath.Join(OutPath, INDEX_FILE)
+		file, err := os.Create(sitePath)
+		if err != nil {
+			return fmt.Errorf("Error creating index file: %s", err)
+		}
+
+		if err := t.Execute(file, posts); err != nil {
+			return fmt.Errorf("Error rendering posts: %s", err)
+		}
+
+		if err := file.Close(); err != nil {
+			return fmt.Errorf("Error closing file: %s", err)
+		}
+
+		return nil
+	},
+}
+
+const (
+	DEFAULT_OUT_PATH = "./out/"
+	INDEX_FILE       = "index.html"
+	TEMPLATE         = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Blog</title>
+	</head>
+	<body>
+		{{range .}}<div>{{ .Rendered }}</div>{{else}}<div><strong>no posts</strong></div>{{end}}
+	</body>
+</html>`
+)
+
+var (
+	OutPath string
+	Force   bool
+)
+
+func init() {
+	rootCmd.AddCommand(renderCmd)
+
+	rootCmd.PersistentFlags().StringVar(
+		&OutPath,
+		"out",
+		DEFAULT_OUT_PATH,
+		"Output folder.",
+	)
+
+	rootCmd.PersistentFlags().BoolVar(
+		&Force,
+		"force",
+		false,
+		"Overwrites an existing output folder.",
+	)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/RaphaelPour/blogctl
 go 1.14
 
 require (
+	github.com/fatih/color v1.9.0
+	github.com/gomarkdown/markdown v0.0.0-20200824053859-8c8b3816f167
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/spf13/cobra v1.0.0
 	github.com/stretchr/testify v1.2.2

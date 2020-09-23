5952f5875c983488140f26c669d647de0a8bc197
Raphael Pour <info@raphaelpour.de>
2020-09-23 21:57:58 +0200

    Testing: Add metadata test

--- pkg/metadata/metadata_test.go
+++ pkg/metadata/metadata_test.go
@@ -1,12 +1,37 @@
 package metadata
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
-
+"fmt"
+"time"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLoadMetadata(t *testing.T) {
+
+	/* Save example metadata json */
+	title := "Exit the known"
+	date := time.Now()
+	data := fmt.Sprintf(
+		`{"title": "%s", "createdAt": "%s"}`,
+		title,
+		date.String(),
+	)
+
+	tmpDir, err := ioutil.TempDir("", "post-")
+	require.Nil(t, err)
+
+	path := GetMetadataPath(tmpDir)
+
+	require.Nil(t, ioutil.WriteFile(path, []byte(data), os.ModePerm))
+
+	meta, err := Load(tmpDir)
+	require.Nil(t, err)
+
+	require.Equal(t, title, meta.Title)
+	require.Equal(t, date, meta.CreatedAt)
 }
 
 func TestNewInvalidMetadata(t *testing.T) {

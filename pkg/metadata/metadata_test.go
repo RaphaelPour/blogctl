126a279b18849e96499f1c29e03d07f26d0a764c
Raphael Pour <info@raphaelpour.de>
2020-09-25 17:05:12 +0200

    Testing: Add metadata tests

--- pkg/metadata/metadata_test.go
+++ pkg/metadata/metadata_test.go
@@ -1,23 +1,20 @@
 package metadata
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
 	"testing"
-"fmt"
-"time"
-	"github.com/stretchr/testify/require"
 )
 
 func TestLoadMetadata(t *testing.T) {
 
 	/* Save example metadata json */
-	title := "Exit the known"
-	date := time.Now()
-	data := fmt.Sprintf(
-		`{"title": "%s", "createdAt": "%s"}`,
-		title,
-		date.String(),
+	title := "99 diets that make you actually fatter"
+	date := 1234
+	data := fmt.Sprintf(`{"title": "%s", "createdAt": %d}`,
+		title, date,
 	)
 
 	tmpDir, err := ioutil.TempDir("", "post-")
@@ -35,9 +32,34 @@ func TestLoadMetadata(t *testing.T) {
 }
 
 func TestNewInvalidMetadata(t *testing.T) {
+	/* Save example metadata json */
+	data := `{"title": "How I lost -5 kgs in one week", "createdAt": 1234`
+
+	tmpDir, err := ioutil.TempDir("", "post-")
+	require.Nil(t, err)
+
+	path := GetMetadataPath(tmpDir)
+
+	require.Nil(t, ioutil.WriteFile(path, []byte(data), os.ModePerm))
+
+	meta, err := Load(tmpDir)
+	require.NotNil(t, err)
+	require.Regexp(t, "^Error parsing metadata:", err)
+	require.Nil(t, meta)
 }
 
 func TestSaveMetadata(t *testing.T) {
+	meta := &Metadata{
+		Title:     "99 problems with PHP",
+		CreatedAt: 788918400,
+	}
+
+	tmpDir, err := ioutil.TempDir("", "post-")
+	require.Nil(t, err)
+
+	err = meta.Save(tmpDir)
+	require.Nil(t, err)
+	require.FileExists(t, GetMetadataPath(tmpDir)
 }
 
 func TestMetadataFile(t *testing.T) {

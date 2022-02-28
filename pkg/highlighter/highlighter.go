c8ca624ce8641575010f7a4bd03016a734db83f6
Tch1b0 <jpour3006@gmail.com>
2022-02-28 13:55:37 +0100

    add highlighter

---
+++ pkg/highlighter/highlighter.go
@@ -0,0 +1,38 @@
+package highlighter
+
+import (
+	"io"
+
+	"github.com/d4l3k/go-highlight"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/html"
+)
+
+func renderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+	// skip if the current node is not a code-block
+	if _, ok := node.(*ast.CodeBlock); !ok {
+		return ast.GoToNext, false
+	}
+
+	// get the language of the code from the top of the code-block
+	codeLanguage := string(node.(*ast.CodeBlock).Info[:])
+
+	// get the raw code from the node
+	rawCode := string(node.AsLeaf().Literal[:])
+	highlightedCode, _ := highlight.HTML(codeLanguage, []byte(rawCode))
+
+	// write the highlighted code
+	w.Write(highlightedCode)
+	return ast.GoToNext, true
+}
+
+// get a html-renderer that adds css classes to different parts
+// of a code-block
+func GetRenderer() *html.Renderer {
+	opts := html.RendererOptions{
+		Flags:          html.CommonFlags,
+		RenderNodeHook: renderHook,
+	}
+
+	return html.NewRenderer(opts)
+}
---
+++ pkg/highlighter/highlighter_test.go
@@ -0,0 +1,24 @@
+package highlighter
+
+import (
+	"testing"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRenderer(t *testing.T) {
+	renderer := GetRenderer()
+	require.IsType(t, &(html.Renderer{}), renderer)
+
+	text := "```go\n" +
+		"func main() {\n" +
+		"	fmt.Println('Test %d', 3)\n" +
+		"	// Comment\n" +
+		"}\n" +
+		"```"
+	result := markdown.ToHTML([]byte(text), nil, renderer)
+	expected := "<div class=\"highlight\"><pre><code><span class=\"keyword\">func</span> main() {\n\tfmt.Println(<span class=\"string\">&#39;Test %d&#39;</span>, <span class=\"number\">3</span>)\n\t<span class=\"comment\">// Comment</span>\n}\n</code></pre></div>"
+	require.Equal(t, expected, string(result[:]))
+}
